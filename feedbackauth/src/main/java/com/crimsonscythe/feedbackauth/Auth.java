6e4cc8c41be4d363ef063a53e02ea228766343e2
CrimsonScythe <crimsonscythestudos@gmail.com>
2019-12-22 16:47:15 +0100

    created new module

---
+++ feedbackauth/src/main/java/com/crimsonscythe/feedbackauth/Auth.java
@@ -0,0 +1,11 @@
+package com.crimsonscythe.feedbackauth;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class Auth {
+
+    public static void Init2 (Context context, String msg) {
+        Toast.makeText(context, msg, Toast.LENGTH_LONG).show();
+    }
+}
